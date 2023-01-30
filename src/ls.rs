3a05c609de9037f99d28734b234c2c23f12d4ae9
MarkusAJacobsen <markusja@stud.ntnu.no>
2023-01-30 20:30:13 +0100

    Trim file extension from ls and added some cosmetics to print

--- src/ls.rs
+++ src/ls.rs
@@ -2,20 +2,25 @@ use std::fs;
 use std::fs::ReadDir;
 use std::path::Path;
 use crate::file_helpers::get_vault_path_string;
+use ansi_term::Colour::{Yellow};
 
 pub(crate) fn ls () {
+    println!("{}", Yellow.underline().paint("Stored secrets:"));
+
     let dir_string = get_vault_path_string();
     let entries: ReadDir;
 
     if Path::exists(dir_string.as_ref()) {
         entries = fs::read_dir(dir_string).unwrap();
     } else {
-        println!("Empty vault");
+        println!("{:ident$}Empty vault", "", ident=2);
         return;
     }
 
     for entry in entries {
         let entry = entry.unwrap();
-        println!("{}", entry.file_name().to_str().unwrap());
+        let file_name = entry.file_name().into_string().unwrap();
+        let path = Path::new(&file_name);
+        println!("{:ident$}{}", "", path.with_extension("").to_str().unwrap(), ident=2);
     }
 }
--- src/new.rs
+++ src/new.rs
@@ -1,9 +1,17 @@
+use std::path::Path;
 use crate::file_helpers::get_vault_path_string;
 
 pub(crate) fn new(name: &String) {
-    let dir_name = get_vault_path_string();
+    let dir_name = & get_vault_path_string();
 
-    let vim_command = "vim ".to_owned() + &*dir_name + "/" + name + ".txt";
+    let file_path_as_string = format!("{}/{}.txt", dir_name, name);
+
+    if Path::exists(file_path_as_string.as_ref()) {
+        println!("Secret '{}' already exists.", name);
+        return;
+    }
+
+    let vim_command = "vim ".to_owned() + dir_name + "/" + name + ".txt";
 
     std::process::Command::new("/usr/bin/sh")
         .arg("-c")
