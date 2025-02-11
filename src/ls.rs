de10757ce923f5253a4f633fc35d12fce8a1a538
MarkusAJacobsen <markus.jacobsen@marcello.no>
2025-02-11 09:50:28 +0100

    Sort the output of ls alphabetically

--- src/ls.rs
+++ src/ls.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::fs::ReadDir;
+use std::fs::{DirEntry, ReadDir};
 use std::path::Path;
 use crate::file_helpers::get_vault_path_string;
 use ansi_term::Colour::{Yellow};
@@ -16,9 +16,13 @@ pub(crate) fn ls () {
         println!("{:ident$}Empty vault", "", ident=2);
         return;
     }
-
-    for entry in entries {
-        let entry = entry.unwrap();
+    
+    let mut entries_as_vector = entries
+        .map(|res|res.unwrap())
+        .collect::<Vec<DirEntry>>();
+    
+    entries_as_vector.sort_by_key(|dir|dir.file_name());
+    for entry in entries_as_vector {
         let file_name = entry.file_name().into_string().unwrap();
         let path = Path::new(&file_name);
         println!("{:ident$}{}", "", path.with_extension("").to_str().unwrap(), ident=2);
